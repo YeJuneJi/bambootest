666b3ee4a0c5729cd5811605a217ed0d7e31980a
YeJuneJi <darksuhak7@gmail.com>
2019-04-22 13:18:21 +0900

    Commit

---
+++ BambooTest/BambooTTest/CalTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BambooTest;
+
+namespace BambooTTest
+{
+    [TestClass]
+    public class CalTest
+    {
+        [TestMethod]
+        public void Add()
+        {
+            Calculate ex = new Calculate();
+            Assert.AreEqual(31, ex.Add(20, 11));
+        }
+
+        [TestMethod]
+        public void Sub()
+        {
+            Calculate ex = new Calculate();
+            Assert.AreEqual(10, ex.Sub(20, 10));
+        }
+
+        [TestMethod]
+        public void Mul()
+        {
+            Calculate ex = new Calculate();
+            Assert.AreEqual(6, ex.Mul(2, 3));
+        }
+
+        [TestMethod]
+        public void Div()
+        {
+            Calculate ex = new Calculate();
+            Assert.AreEqual(6, ex.Div(48, 8));
+        }
+    }
+}
--- BambooTest/BambooTest/Program.cs
+++ BambooTest/BambooTest/Program.cs
@@ -11,4 +11,31 @@ namespace BambooTest
         {
         }
     }
+
+    public class Calculate
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            if (a < b)
+            {
+                int temp = 0;
+                temp = a;
+                a = b;
+                b = a;
+            }
+            return a / b;
+        }
+    }
 }
